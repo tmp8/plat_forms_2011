d8b71d1620f38f1c2aa653345d09642acb66ede9
unnu <norman.timmler@gmail.com>
2011-01-18 12:52:53 +0100

    * last time added db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(:version => 20110118100519) do
 
-  create_table "friendships", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.integer  "status",            :default => 0
-    t.datetime "status_changed_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "friendships", ["friend_id"], :name => "index_friendships_on_friend_id"
-  add_index "friendships", ["user_id"], :name => "index_friendships_on_user_id"
-
   create_table "conferences", :force => true do |t|
     t.string   "version"
     t.string   "name"
@@ -44,6 +32,18 @@ ActiveRecord::Schema.define(:version => 20110118100519) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "friendships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.integer  "status",            :default => 0
+    t.datetime "status_changed_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "friendships", ["friend_id"], :name => "index_friendships_on_friend_id"
+  add_index "friendships", ["user_id"], :name => "index_friendships_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
