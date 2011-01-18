bb37748ac14a2e48ca0f180ea017234ad5315357
Thies C. Arntzen <thies@tmp8.de>
2011-01-18 15:25:37 +0100

    * added Signin and -out * added flash-partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def login_navigation
+    if current_user
+      link_to("[Sign Out]", destroy_user_session_path)
+    else
+      link_to("[Sign In]", new_user_session_path) + link_to("[Register]", new_user_registration_path) 
+    end
+  end
 end
