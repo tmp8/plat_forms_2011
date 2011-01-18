f6d4d3628ef4f40046d9c7abc1ffa4a8436545ca
unnu <norman.timmler@gmail.com>
2011-01-18 11:01:33 +0100

    * working json parsing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# origin: RM
+data = JSON.parse(IO.read(File.dirname(__FILE__) + '/data.txt'))
+users, foo, conferences, categories, conference_contact = data
+
+p users
