3f9ca8bfa1cfb3ce3861226d7bb601a0da6fb315
Thies C. Arntzen <thies@tmp8.de>
2011-01-18 16:23:45 +0100

    * added user_info helper (respects the privacy)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       link_to("[Sign In]", new_user_session_path) + link_to("[Register]", new_user_registration_path) 
     end
   end
+  
+  def user_info(user)
+    if current_user && ((user == current_user) || (current_user.friend_state(user) == User::IN_CONTACT))
+      "#{user.full_name} (#{user.email})"
+    else
+      "#{user.username}"
+    end
+  end
 end
