63d200adb6cbbfb0a5acb0e6a5d5c4d16546e2f3
unnu <norman.timmler@gmail.com>
2011-01-19 13:45:52 +0100

    * show friendships in profile

--- app/controllers/friendship_requests_controller.rb
+++ app/controllers/friendship_requests_controller.rb
@@ -2,11 +2,14 @@ class FriendshipRequestsController < ApplicationController
   before_filter :authenticate_user!
   
   def send_many
+    friends = []
     current_user.transaction do
       params[:user].keys.each do |new_friend_id|
+        friend = User.find(new_friend_id)
+        friends << friend
         current_user.request_friendship(User.find(new_friend_id))
       end
     end
-    redirect_to :back, :notice => "Request(s) sent!"
+    redirect_to :back, :notice => "Friendship request sent to #{friends.map(&:username).join(', ')}!"
   end
 end
