29bda824ad4ad514c7b0cfa156866a308e0d08aa
unnu <norman.timmler@gmail.com>
2011-01-19 08:21:49 +0100

    * completed user validations

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -8,7 +8,11 @@ class UserTest < ActiveSupport::TestCase
       @user = Factory(:user)
     end
     
+    should validate_presence_of(:full_name)
+    should validate_presence_of(:email)
     should validate_presence_of(:username)
+    should validate_presence_of(:city)
     should validate_presence_of(:country)
+    should validate_uniqueness_of :username
   end
 end
