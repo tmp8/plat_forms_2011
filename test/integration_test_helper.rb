b54b6db1ba5f380f49fd510106e5576c9b512ca3
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2011-01-19 09:00:21 +0100

    fix integration test login

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -9,8 +9,8 @@ def sign_in(user)
   visit new_user_session_path
   fill_in 'Login', :with => user.username
   fill_in 'Password', :with => "123456"
-  
-  click_link_or_button('Sign in')
+
+  click_link_or_button('user_submit')
 end
 
 module ActionController
